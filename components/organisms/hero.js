3c749a22a4f086bb6df3a56ea058a78ba4cdaf4a
wulandarisri268 <wulandarisri268@gmail.com>
2023-07-28 21:49:30 +0700

    style: fix banner text-alignment

--- components/organisms/hero.js
+++ components/organisms/hero.js
@@ -5,8 +5,8 @@ export default function Hero() {
     return (
         <main className="lg:grid grid-cols-2 place-items-center gap-5 mt-16 lg:mt-9 p-5 md:px-6 px-4">
             <div className="col-span-1 lg:pl-5 text-center lg:text-left">
-                <h1 className="md:w-11/12 text-2xl md:text-3xl lg:text-4xl font-bold">Stay Informed, Stay Safe: Tracking COVID-19 Statistics in Indonesia </h1>
-                <h4 className="md:mt-6 mt-5 lg:mx-0 mx-auto sm:w-11/12 w-full">
+                <h1 className="md:w-11/12 mx-auto lg:mx-0 text-2xl md:text-3xl lg:text-4xl font-bold">Stay Informed, Stay Safe: Tracking COVID-19 Statistics in Indonesia </h1>
+                <h4 className="md:mt-6 mt-5 lg:text-left lg:mx-0 mx-auto sm:w-11/12 w-full">
                     Welcome to our COVID-19 Information Center, your one-stop destination for real-time updates and comprehensive statistics on the
                     COVID-19 situation in Indonesia. In these challenging times, staying informed is crucial, and we are committed to providing you
                     with accurate and up-to-date data straight from reliable sources. Our mission is to empower you with knowledge, enabling you to
