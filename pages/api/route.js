f96e33977f74028e7a2eaffd3e41b3cf7dc539ce
wulandarisri268 <wulandarisri268@gmail.com>
2023-07-29 09:29:38 +0700

    feat: add .env.example

--- pages/api/route.js
+++ pages/api/route.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const covidApi = axios.create({
-    baseURL: "https://covid19.keponet.com",
-});
-
-const countriesApi = axios.create({
-    baseURL: "https://restcountries.com",
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 export const getGlobal = async () => {
@@ -17,5 +13,5 @@ export const getCountry = async (country) => {
 };
 
 export const getListCountries = async () => {
-    return await countriesApi.get(`/v3.1/all?fields=name,flags`);
+    return await axios.get(`https://restcountries.com/v3.1/all?fields=name,flags`);
 };
